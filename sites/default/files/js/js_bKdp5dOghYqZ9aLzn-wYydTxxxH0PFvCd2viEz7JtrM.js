24e8c78e26669d19cc9cbd19da8d70a27d84c1f8
Mirella Acero <mirellacero@gmail.com>
2024-11-06 14:45:33 +0100

    refresh to the page

--- sites/default/files/js/js_bKdp5dOghYqZ9aLzn-wYydTxxxH0PFvCd2viEz7JtrM.js
+++
@@ -1,2 +0,0 @@
-var $zoho= $zoho || {salesiq:{values:{},ready:function(){$zoho.salesiq.floatbutton.visible("hide");}}};var d=document;s=d.createElement("script");s.type="text/javascript";s.defer=true;s.src="https://salesiq.zoho.com/institutoavansys/float.ls?embedname=institutoavansys";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);
-;
--- sites/default/files/js_injector/js_injector_5154d.js
+++
@@ -1,4 +0,0 @@
-setInterval(function(){ 
-jQuery('#btn-block-block-31').addClass('boton-popup');
-jQuery('#btn-block-webform-client-block-130').addClass('boton-popup');
-}, 10);
